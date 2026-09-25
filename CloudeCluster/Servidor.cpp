0273951c056b50bf53cc6dcf613171d9c8bf3f4b
Dulce-Vargas <vargasoriasd@gmail.com>
2026-09-25 11:58:07 -0600

    clase servidor

--- CloudeCluster/Servidor.cpp
+++ CloudeCluster/Servidor.cpp
@@ -1,2 +1,32 @@
 #include "Servidor.h"
-//metodos
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+Servidor::Servidor() {
+	id = 0;
+	nombre = "";
+	ip = "";
+	arquitectura = "";
+	siguiente = NULL;
+	anterior = NULL;
+	cola = NULL;
+}
+int Servidor::getId() {
+	return id;
+}
+string Servidor::getNombre() {
+	return nombre;
+}
+string Servidor::getIp() {
+	return ip;
+}
+string Servidor::getArquitectura() {
+	return arquitectura;
+}
+void Servidor::setNombre(string nuevoNombre) {
+	nombre = nuevoNombre;
+}
+void Servidor::setArquitectura(string nuevaArquitectura) {
+	arquitectura = nuevaArquitectura;
+}
